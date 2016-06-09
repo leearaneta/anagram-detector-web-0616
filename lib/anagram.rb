6ef69bae8d3110256e5b66fc8ccdb8540da4e769
leearaneta <lee.araneta@gmail.com>
2016-06-09 18:10:57 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+	def initialize(word)
+		@word = word
+	end
+
+	def match(string)
+		string.select do |letters| 
+			letters.split("").sort.join == @word.split("").sort.join
+		end
+	end
+end
